b4a5dbd008fa77b7cb4975e6209ff548605b5c4f
Marc Binder <marcandrebinder@gmail.com>
2021-05-17 16:10:32 +0200

    feat: upsert version

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,54 @@ import * as github from '@actions/github'
 import * as util from './util'
 import * as config from './config'
 
+interface Release {
+  id: number
+  tag_name: string
+  upload_url: string
+  html_url: string
+}
+
 async function run(): Promise<void> {
   try {
     const options = config.read()
     const ok = github.getOctokit(options.token)
 
+    const owner = github.context.repo.owner
+    const repo = github.context.repo.repo
     const version = await util.determineVersion(options.versionFile)
 
-    const release = await ok.rest.repos.createRelease({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      tag_name: version.raw,
-      prerelease: version.prerelease.length > 0
-    })
+    let release: null | Release = null
 
-    core.setOutput('id', release.data.id)
-    core.setOutput('tag_name', release.data.tag_name)
-    core.setOutput('upload_url', release.data.upload_url)
-    core.setOutput('html_url', release.data.html_url)
+    const existingRelease = await ok.rest.repos.getReleaseByTag({
+      owner,
+      repo,
+      tag: version.raw
+    })
+    if (existingRelease.status === 200) {
+      release = existingRelease.data
+    }
+
+    if (!release) {
+      const createdRelease = await ok.rest.repos.createRelease({
+        owner,
+        repo,
+        tag_name: version.raw,
+        prerelease: version.prerelease.length > 0
+      })
+
+      if (createdRelease.status === 201) {
+        release = createdRelease.data
+      }
+    }
+
+    if (!release) {
+      throw new Error('Unable to create release')
+    }
+
+    core.setOutput('id', release.id)
+    core.setOutput('tag_name', release.tag_name)
+    core.setOutput('upload_url', release.upload_url)
+    core.setOutput('html_url', release.html_url)
   } catch (err) {
     if (err instanceof config.InvalidConfigError) {
       core.setFailed(`invalid configuration: ${err.message}`)
